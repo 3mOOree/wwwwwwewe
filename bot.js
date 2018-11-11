75f23074949a57114074b4baada8a2f51dd3d829
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-11 22:05:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -205,7 +205,8 @@ Role : __${ar[message.guild.id].role}__`)
 
     fs.writeFile("./AutoRole.json", JSON.stringify(ar), (err) => {
     if (err) console.error(err)
-  });
+  }
+});                
 
 
 
