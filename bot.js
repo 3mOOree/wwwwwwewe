d5f478db70e87fa2af803de199fc8f400938c811
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-11 18:31:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ const code = '#';
 client.on('message',async message => {
     if(message.content.startsWith(code + "js")) {
   if(!message.channel.guild) return message.reply(' ');
-    let rank = message.guild.member(message.author).roles.find(`name`, "• Superme » Support");
+    let rank = message.guild.member(message.author).roles.find('name', '• Superme » Support');
     if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة Superme » Support لأستخدام هذا الأمر.**');
     let jscodes = message.guild.channels.find(`name`, "✽-discord-js");
     if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
