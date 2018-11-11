3a462b528b577c539fd283737d66ec19dceec14a
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-11 20:08:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,6 +106,27 @@ ${thisMessage}\`\`\`
 );
       })}});
 
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('511239519144771614', 'ChannelID').setName("「");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Wel");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welco");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To ");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To S");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Su);
+client.channels.find('id', 'ChannelID').setName("「 Welcome To Sup");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Supe");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Super );
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Super me co");
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Superme Codes 」");
+  }, 3000);
+});
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN)
