47959289489a86d7574644abd98a4b5ba3e1eba3
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-11 20:45:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,6 +112,32 @@ ${thisMessage}\`\`\`
 
 
 
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('$' + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('$' + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('$' + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  }
+
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN)
 
