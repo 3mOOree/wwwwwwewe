d19c685159dc47d6e73f01b28c9fa96fee897c84
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-11 20:13:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To 
 client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Su");
 client.channels.find('id', 'ChannelID').setName("「 Welcome To Sup");
 client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Supe");
-client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Super );
+client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Super" );
 client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Super me co");
 client.channels.find('511239519144771614', 'ChannelID').setName("「 Welcome To Superme Codes 」");
   }, 3000);
