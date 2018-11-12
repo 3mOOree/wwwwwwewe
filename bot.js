3403886cdacea02571ed8d906d3ea354beb7d093
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-12 19:40:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,63 +19,75 @@ client.on('ready', () => {
     client.user.setActivity("#js",{type: 'WATCHING'}); //Heeeeeemo Heeeeeeeeeemo 
 });
 
-const code = '#';
+const code = '!';
+
 client.on('message',async message => {
-  if(message.content.startsWith(code + "js")) {
-    if(!message.channel.guild) return message.reply(' ');
+    if(message.content.startsWith(code + "js")) {
+  if(!message.channel.guild) return message.reply(' ');
     let rank = message.guild.member(message.author).roles.find('name', '• Superme » Support');
-    if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة • Superme » Support لأستخدام هذا الأمر.**');
+    if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة • Superme » Support  لأستخدام هذا الأمر.**');
     let jscodes = message.guild.channels.find(`name`, "✽-discord-js");
     if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
-    let filter = m => m.author.id === message.author.id;
-    let thisMessage;
-    let thisFalse;
-    message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
-    message.channel.awaitMessages(filter, {
-      max: 1,
-      time: 90000,
-      errors: ['time']
-    })
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+  
+      message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 90000,
+        errors: ['time']
+      })
       .then(collected => {
         collected.first().delete();
         thisMessage = collected.first().content;
         let boi;
         msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
-        message.channel.awaitMessages(filter, {
-          max: 1,
-          time: 90000,
-          errors: ['time']
-        })
-          .then(collected => {
-            collected.first().delete();
-            boi = collected.first().content;
-            let boi2;
-            msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
+  
             message.channel.awaitMessages(filter, {
               max: 1,
               time: 90000,
               errors: ['time']
             })
-              .then(collected => {
-                collected.first().delete();
-                boi2 = collected.first().content;
-                msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
-                message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+            .then(collected => {
+              collected.first().delete();
+              boi = collected.first().content;
+              let boi2;
+              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
+  
+                message.channel.awaitMessages(filter, {
                   max: 1,
                   time: 90000,
                   errors: ['time']
                 })
-                  .then(collected => {
-                    if(collected.first().content === 'لا') {
-                      msg.delete();
-                      message.delete();
-                      thisFalse = false;
-                    }
-                   if(collected.first().content === 'نعم') {
-                     if(thisFalse === false) return;
-                     msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
-                     collected.first().delete();
-                     jscodes.send("@everyone | @here\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n**Superme Codes© :arrow_down:**\n\`\`\`js"+thisMessage+"\`\`\`\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n**وصف الكود**: \n"boi"\n**تم النشر بواسطة**: "+message.author+"\n**المصدر / الشخص الذي صنع الكود**: +"boi2+'"');
+                .then(collected => {
+                  collected.first().delete();
+                boi2 = collected.first().content;
+        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+            collected.first().delete();
+            jscodes.send(`@everyone | @here
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**Superme Codes© :arrow_down:**            
+\`\`\`js
+${thisMessage}\`\`\`
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**وصف الكود**: ${boi}
+**تم النشر بواسطة**: ${message.author}
+**المصدر / الشخص الذي صنع الكود**: ${boi2}`);
                    }
                  });
                });
