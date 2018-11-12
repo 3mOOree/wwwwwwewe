777f57cb5377c0b59cc6f1aa3c3bd9f92d307778
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-12 19:30:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,12 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = '#'
 
-
-
-
 client.on('ready', () => {
     console.log('╔[════════════════════════════════════]╗'); // Heeeeeemo
     console.log('')
-    console.log('            ╔[════════════]╗')
-    console.log('              Bot Is Online')
-    console.log('            ╚[════════════]╝')
+    console.log(' ╔[════════════]╗')
+    console.log(' Bot Is Online')
+    console.log(' ╚[════════════]╝')
     console.log('')
     console.log(`Logged in as ${client.user.tag}!`);
     console.log('')
@@ -19,95 +16,78 @@ client.on('ready', () => {
     console.log(`Users! [ " ${client.users.size} " ]`);
     console.log('')
     console.log('╚[════════════════════════════════════]╝')
-          client.user.setActivity("#js",{type: 'WATCHING'});     //Heeeeeemo Heeeeeeeeeemo   
+    client.user.setActivity("#js",{type: 'WATCHING'}); //Heeeeeemo Heeeeeeeeeemo 
 });
 
-
-
-
 const code = '#';
-
 client.on('message',async message => {
-    if(message.content.startsWith(code + "js")) {
-  if(!message.channel.guild) return message.reply(' ');
+  if(message.content.startsWith(code + "js")) {
+    if(!message.channel.guild) return message.reply(' ');
     let rank = message.guild.member(message.author).roles.find('name', '• Superme » Support');
     if (!rank) return message.channel.send(':octagonal_sign: **| يجب ان تمتلك رتبة • Superme » Support لأستخدام هذا الأمر.**');
     let jscodes = message.guild.channels.find(`name`, "✽-discord-js");
     if(!jscodes) return message.channel.send(":x:لم اجد الروم الخاص بنشر الاكواد");
-      let filter = m => m.author.id === message.author.id;
-      let thisMessage;
-      let thisFalse;
-      message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
-  
-      message.channel.awaitMessages(filter, {
-        max: 1,
-        time: 90000,
-        errors: ['time']
-      })
+    let filter = m => m.author.id === message.author.id;
+    let thisMessage;
+    let thisFalse;
+    message.channel.send(':pencil: **| من فضلك اكتب الكود الأن... :pencil2: **').then(msg => {
+    message.channel.awaitMessages(filter, {
+      max: 1,
+      time: 90000,
+      errors: ['time']
+    })
       .then(collected => {
         collected.first().delete();
         thisMessage = collected.first().content;
         let boi;
         msg.edit(':scroll: **| من فضلك اكتب وصف الكود الأن... :pencil2: **').then(msg => {
-  
+        message.channel.awaitMessages(filter, {
+          max: 1,
+          time: 90000,
+          errors: ['time']
+        })
+          .then(collected => {
+            collected.first().delete();
+            boi = collected.first().content;
+            let boi2;
+            msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
             message.channel.awaitMessages(filter, {
               max: 1,
               time: 90000,
               errors: ['time']
             })
-            .then(collected => {
-              collected.first().delete();
-              boi = collected.first().content;
-              let boi2;
-              msg.edit(':man_in_tuxedo: **| من فضلك اكتب من صنع هذا الكود الأن... :pencil2: **').then(msg => {
-  
-                message.channel.awaitMessages(filter, {
+              .then(collected => {
+                collected.first().delete();
+                boi2 = collected.first().content;
+                msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
+                message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
                   max: 1,
                   time: 90000,
                   errors: ['time']
                 })
-                .then(collected => {
-                  collected.first().delete();
-                boi2 = collected.first().content;
-        msg.edit(':shield: **| [ هل انت متأكد من نشر الكود؟ | [ نعم ] او [ لا**');
-   message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
-          max: 1,
-          time: 90000,
-          errors: ['time']
-        })
-        .then(collected => {
-          if(collected.first().content === 'لا') {
-            msg.delete();
-            message.delete();
-            thisFalse = false;
-          }
-          if(collected.first().content === 'نعم') {
-            if(thisFalse === false) return;
-            msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
-            collected.first().delete();
-            jscodes.send(`@everyone | @here
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-**Superme Codes© :arrow_down:**            
-\`\`\`js
-${thisMessage}\`\`\`
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-**وصف الكود**: ${boi}
-**تم النشر بواسطة**: ${message.author}
-**المصدر / الشخص الذي صنع الكود**: ${boi2}`); 
-         }
-        }
-    );
+                  .then(collected => {
+                    if(collected.first().content === 'لا') {
+                      msg.delete();
+                      message.delete();
+                      thisFalse = false;
+                    }
+                   if(collected.first().content === 'نعم') {
+                     if(thisFalse === false) return;
+                     msg.edit(':dove: **| Done :white_check_mark:, تم بنجاح نشر كودك في روم الاكواد**');
+                     collected.first().delete();
+                     jscodes.send("@everyone | @here\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n**Superme Codes© :arrow_down:**\n\`\`\`js"+thisMessage+"\`\`\`\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n**وصف الكود**: \n"boi"\n**تم النشر بواسطة**: "+message.author+"\n**المصدر / الشخص الذي صنع الكود**: +"boi2+'"');
+                   }
+                 });
+               });
+             });
+           });
+         });
+       });
+     });
+   }
 });
-      });
-    }
-      );
-    });
-}
-);
-      
 
-
-client.on('message', async message => {
+client.on('message', async (message) => {
   if(message.content.startsWith("#تقديم")) {
     await message.channel.send("** ما لغتك؟**").then(e => {
     let filter = m => m.author.id === message.author.id
@@ -115,68 +95,45 @@ client.on('message', async message => {
     let md = '';
     let br = '';
     let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
-    .then(collected => {
-      lan = collected.first().content
-      collected.first().delete()
-e.delete();
-     message.channel.send('** ما خبرتك؟**').then(m => {
-let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
-.then(co => {
-  md = co.first().content
-        co.first().delete()
-        m.delete();
-message.channel.send('**من متي وانت تبرمج؟**').then(ms => {
-let br = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
-.then(col => {
-  br = col.first().content
-        col.first().delete()
-
-ms.delete()
-
- message.channel.send('جاري التقديم ..').then(b => {
-        setTimeout(() => {
-  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
-        },2000);
-var gg = message.guild.channels.find('name', '✻-التقديمات')
-if(!gg) return;
-if(gg) {
-gg.send({embed : new Discord.RichEmbed()
-.setDescription(`**  اللغة :question:  : \n ${lan}\nالخبرة :link: :\n ${md} \nالمدة :question: :\n ${br}  \nتم التقديم بواسطة  : <@${message.author.id}> **`)  
-          .setFooter(`Superme™`)
-.setTimestamp()
-});
-}        
+      .then(collected => {
+        lan = collected.first().content
+        collected.first().delete()
+        e.delete();
+        message.channel.send('** ما خبرتك؟**').then(m => {
+        let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
+          .then(co => {
+            md = co.first().content
+            co.first().delete()
+            m.delete();
+            message.channel.send('**من متي وانت تبرمج؟**').then(ms => {
+            let br = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
+              .then(col => {
+                br = col.first().content
+                col.first().delete()
+                ms.delete()
+                message.channel.send('جاري التقديم ..').then(b => {
+                setTimeout(() => { 
+                  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
+                },2000);
+                var gg = message.guild.channels.find('name', '✻-التقديمات')
+                if(!gg) return;
+                if(gg) {
+                  gg.send({
+                      embed : new Discord.RichEmbed()
+                      .setDescription(`** اللغة :question: : \n ${lan}\nالخبرة :link: :\n ${md} \nالمدة :question: :\n ${br} \nتم التقديم بواسطة : <@${message.author.id}> **`)
+                      .setFooter(`Superme™`)
+                      .setTimestamp()
+                    });
+                  } 
+                })
+              })
+            })
+          })
+        })
+      })
+    })
+  }
 })
-})
-})
-})
-})
-})
-})
- }
-})
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
 
 
 client.login(process.env.BOT_TOKEN)
