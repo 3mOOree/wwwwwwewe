97d4141aea02ba010f854ac9fb155958dc72302c
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-18 19:01:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -507,6 +507,31 @@ client.on("guildMemberAdd", async member => {
 
 
  
+client.on('message', message => {
+var prefix = "-"
+    if (message.content.startsWith(prefix + 'clear')) {
+      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(`ليس لديك برمشن[*MANAGE_MESSAGES*] `).catch(console.error);
+  message.delete()
+  if(!message.channel.guild) return;
+  let args = message.content.split(" ").slice(1);
+  
+  const messagecount = parseInt(args.join(' '));
+  
+  message.channel.fetchMessages({
+  
+  limit: messagecount
+  
+  }).then(messages => message.channel.bulkDelete(messages));
+  message.channel.sendMessage("", {embed: {
+    title: "``✏️✅ تــم مسح الشات ``",
+    color: 0x06DF00,
+    footer: {
+    
+    }
+    }}).then(msg => {msg.delete(3000)});
+  };
+  
+  });
 
 
 
