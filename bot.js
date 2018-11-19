26c67578e9bb562a2a98bfbcdec8df541656efe0
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-19 19:19:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -512,28 +512,24 @@ var prefix = "."
   });
 
 
-client.on('message',async msg => {
-  var p = "!";
-  if(msg.content.startsWith(p + "user")) {
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
-  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
-    time.overwritePermissions(msg.guild.id, {
+
+client.on('message',async message => {
+  if(message.content.startsWith("!user")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+  message.channel.send('✅| **تم عمل الروم بنجاح**');
+  message.guild.createChannel(`Voice Online : [ ${client.users.size} ]` , 'voice').then(c => {
+    console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
+    c.overwritePermissions(message.guild.id, {
       CONNECT: false,
       SPEAK: false
     });
-  setInterval(() => {
-      var currentTime = new Date(),
-Year = currentTime.getFullYear(),
-Month = currentTime.getMonth() + 1,
-Dat = currentTime.getDate()
-      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
- },1000);
+    setInterval(() => {
+      c.setName(`Members : [ ${client.users.size} ]`)
+    },1000);
   });
   }
- 
 });
-
 
 
 client.login(process.env.BOT_TOKEN)
