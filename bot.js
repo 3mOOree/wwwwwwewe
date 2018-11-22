085c2f307badb989a57cb01f7a5caadc750a16ce
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-11-22 21:02:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -523,7 +523,41 @@ Dat = currentTime.getDate()
   } 
 });
 
+client.on('message', message => {
+   if(message.content.startsWith("رابط")) {
+        
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 86400
+    }).then(invite =>  
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("000000").setColor('#36393e')
+        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
+        .setFooter("Superme Codes™")
+      message.channel.sendEmbed(embed)
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("000000").setColor('#36393e')
+        .setDescription(`
+**-------------------
+-هذا هو الرابط : https://discord.gg/CACFj5y
 
+https://discord.gg/wK3zjxC
+
+https://discord.gg/bQetqRz
+
+https://discord.gg/DtdzSHu
+
+https://discord.gg/QcngWpY
+-ارسله للي تحب وحيآك انت وياه
+-ونورنا ياجميل :heart: 
+------------------- **`)
+        .setFooter("S.C")
+      message.author.sendEmbed(Embed11)
+    }
+});
 
 
 
