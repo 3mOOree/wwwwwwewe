c2e5477168561ec06288819c30e34cc20b0e55ac
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-12-14 13:28:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -817,6 +817,39 @@ client.on('message', async (message) => {
 })				
 			
 
+client.on('msg', msg => {
+
+  if (msg.content === 'رابط') {
+
+  	    msg.react("💜")
+
+  	let embed = new Discord.RichEmbed()
+
+  	.setColor("RANDOM")
+
+   .setTitle(`تفضل يا قلبي 🌹`)
+
+   .setDescription(` 
+
+https://discord.gg/wK3zjxC
+
+https://discord.gg/bQetqRz
+
+https://discord.gg/DtdzSHu
+
+https://discord.gg/QAsfMqQ
+
+`)
+
+.setFooter(`Superme™`)
+
+msg.channel.sendEmbed(embed);
+
+}
+
+});  
+
+    
           
               
 
