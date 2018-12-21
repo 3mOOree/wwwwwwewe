8ac191e366bd08f412e026fd4dc000da8c5b2e8b
3mOOree <43295008+3mOOree@users.noreply.github.com>
2018-12-21 23:09:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -568,7 +568,7 @@ var prefix = ">";
 if(message.channel.type === "dm") return;
 if(message.author.bot) return;
   if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
-    channel: "❃-welcome"
+    channel: "・welcome"
 }
 const channel = sWlc[message.guild.id].channel
   if (message.content.startsWith(prefix + "setwelcomer")) {
@@ -581,7 +581,7 @@ const channel = sWlc[message.guild.id].channel
 });
 client.on("guildMemberAdd", member => {
       if(!sWlc[member.guild.id]) sWlc[member.guild.id] = {
-    channel: "❃-welcome"
+    channel: "・welcome"
   }
   const channel = sWlc[member.guild.id].channel
     const sChannel = sWlc[member.guild.id].channel
